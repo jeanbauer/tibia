fdb6befbba82782100611b495a82c2e854056e3f
Erich Menezes <emenezes@thoughtworks.com>
2020-05-08 19:57:13 -0300

    Backpack uses method that handle all coins :sparkles:

--- src/components/backpack/backpack.js
+++ src/components/backpack/backpack.js
@@ -1,21 +1,27 @@
-import React from "react";
+import React, { useMemo } from "react";
 
 import Item from "../item/item";
 import backpack from "../../images/coin/backpack.gif";
-import gold from "./coins/gold";
+import calculateCoins from "./coins";
 
 import "./backpack.css";
 
 function Backpack(props) {
-  const getCoinImage = () => gold(props.gold);
+  const memoizedCalculateCoins = useMemo(() => calculateCoins(props.gold), [props.gold]);
 
   return (
     <Item name="Backpack" icon={backpack}>
       <div className="flex wrap">
-        <div className="flex center backpack__slot">
-          <img src={getCoinImage()} alt="" />
-          <span className="text-shadow">{props.gold}</span>
-        </div>
+        {
+          memoizedCalculateCoins
+            .filter(({ amount }) => amount > 0)
+            .map(({ asset, amount }, i) => (
+              <div key={i} className="flex center backpack__slot">
+                <img src={asset} alt="" />
+                <span className="text-shadow">{amount}</span>
+              </div>
+            ))
+        }
       </div>
     </Item>
   );
